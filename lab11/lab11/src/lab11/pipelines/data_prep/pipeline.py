12e6d1628965b4baafaa97781c87e6e74a779b77
nicoacevedor <nico.acevedo.rivas@gmail.com>
2023-11-24 19:59:26 -0300

    pipeline creado

--- lab11/lab11/src/lab11/pipelines/data_prep/pipeline.py
+++ lab11/lab11/src/lab11/pipelines/data_prep/pipeline.py
@@ -3,8 +3,34 @@ This is a boilerplate pipeline 'data_prep'
 generated using Kedro 0.18.14
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import get_data, preprocess_companies, preprocess_shuttles, create_model_input_table
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=get_data,
+            inputs=None,
+            outputs=["companies", "shuttles", "reviews"],
+            name="get_data"
+        ),
+        node(
+            func=preprocess_companies,
+            inputs="companies",
+            outputs="companies_prep",
+            name="preprocess_companies"
+        ),
+        node(
+            func=preprocess_shuttles,
+            inputs="shuttles",
+            outputs="shuttles_prep",
+            name="preprocess_shuttles"
+        ),
+        node(
+            func=create_model_input_table,
+            inputs=["shuttles_prep", "companies_prep", "reviews"],
+            outputs="data_prepared",
+            name="create_model_input_table"
+        )
+    ])

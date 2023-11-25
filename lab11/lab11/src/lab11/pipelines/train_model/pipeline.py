a3dd61686eb17e3952cf96ab1e31f0bbbb120066
nicoacevedor <nico.acevedo.rivas@gmail.com>
2023-11-25 02:39:58 -0300

    train_model, sin la función train model

--- lab11/lab11/src/lab11/pipelines/train_model/pipeline.py
+++ lab11/lab11/src/lab11/pipelines/train_model/pipeline.py
@@ -3,8 +3,28 @@ This is a boilerplate pipeline 'train_model'
 generated using Kedro 0.18.14
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import split_data, evaluate_model, get_best_model, train_model
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=split_data,
+            inputs="prepared_data",
+            outputs=["X_train", "X_valid", "X_test", "y_train", "y_valid", "y_test"],
+            name="split_data"
+        ),
+        node(
+            func=train_model,
+            inputs=["X_train", "X_valid", "y_train", "y_valid"],
+            outputs="best_model",
+            name="train_model"
+        ),
+        node(
+            func=evaluate_model,
+            inputs="best_model",
+            outputs=None,
+            name="evaluate_model"
+        )
+    ])
